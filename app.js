2126186113a02dbf01df2ee9b55b6777dfd24150
Roman Matveev <roman.x@live.com>
2018-06-18 22:52:31 -0400

    Cleaned up campground page, added comments, added login/register functionality.

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-var express    = require("express"),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    app        = express();
+var express               = require("express"),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+    passport              = require('passport'),
+    LocalStrategy         = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    app                   = express();
 
 var Campground = require('./models/campground'),
+    Comment    = require('./models/comment'),
+    User       = require('./models/user'),
     seedDB     = require('./seeds');
 
+mongoose.connect("mongodb://localhost/campus");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost/campus");
+app.use(express.static(__dirname + "/public"));
 seedDB();
+
+
+app.use(require('express-session') ({
+    secret: ",F[bU,W+9PHy/4jYBrMe",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -20,9 +44,13 @@ app.get("/campgrounds", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     });
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("campgrounds/new");
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -40,10 +68,6 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
-});
-
 app.get("/campgrounds/:id", function(req, res) {
     Campground.findById(req.params.id).populate("comments").exec(
         function(err, foundCampground) {
@@ -51,10 +75,76 @@ app.get("/campgrounds/:id", function(req, res) {
                 console.log(err);
             } else {
                 console.log(foundCampground);
-                res.render("show", {campground:foundCampground});
+                res.render("campgrounds/show", {campground: foundCampground});
             }
     });
 });
+
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUsername = new User({username: req.body.username});
+    var newPassword = req.body.password;
+    User.register(newUsername, newPassword, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        } passport.authenticate("local") (req, res, function() {
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res) {});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } res.redirect("/login");
+}
 
 app.listen(8000, function() {
     console.log("Running server on port 8000.");
--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ var Comment    = require('./models/comment');
 var data = [
     {
         name: "Cloud's Rest",
-        image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec456c4aeb71d3aecbe65e586d186ec0&auto=format&fit=crop&w=900&q=60",
+        image: "https://i.imgur.com/vzVo319.jpg",
         desc: "Si aliqua doctrina despicationes, nisi quamquam aut domesticarum, ubi quorum fabulas voluptate qui legam commodo est aute elit. Singulis arbitrantur qui vidisse, deserunt hic minim, voluptate do magna, veniam relinqueret commodo illum possumus, constias enim nulla e enim aut fugiat ullamco mentitum qui si minim reprehenderit, cillum possumus iis anim quem. Cupidatat e tempor eu doctrina quo quem mentitum."
     },
     {
         name: "Desert Mesa",
-        image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d95171e276fbd03de651f9aecb64b53d&auto=format&fit=crop&w=900&q=60",
+        image: "https://i.imgur.com/noF1JHh.jpg",
         desc: "Tamen ne senserit ex malis exercitation litteris ipsum pariatur ab esse quamquam admodum te eram doctrina singulis, nescius aliqua quibusdam, iis sint ipsum e constias et summis exquisitaque expetendis illum quibusdam ab sunt iis possumus est culpa. Sunt voluptate non occaecat in minim ubi eiusmod ubi sunt. Ad quem ullamco possumus de quo possumus exercitation, vidisse et duis fabulas, est nescius o singulis, constias export offendit deserunt, export eiusmod singulis, litteris elit iis proident concursionibus de expetendis id nulla nescius."
     },
     {
         name: "Canyon Floor",
-        image: "https://images.unsplash.com/photo-1499363145340-41a1b6ed3630?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3761b04cf7c21b831647fbef86ad438&auto=format&fit=crop&w=900&q=60",
+        image: "https://i.imgur.com/WCUXEWo.jpg",
         desc: "Iis sed amet fore quid ne si eram probant voluptatibus aut et ullamco ad eiusmod nam quem do eu summis deserunt ne o litteris efflorescere iis ab ut relinqueret, se do relinqueret e senserit o labore. De esse cernantur incididunt est cernantur ne nescius, nisi incurreret despicationes, laborum an esse commodo, nam aute arbitrantur, elit si ita labore cernantur, dolor deserunt aut quae irure, quibusdam veniam anim ne export. Pariatur ab dolor an aute do ad irure singulis do a export admodum hic ut nulla singulis laborum, ingeniis labore labore quo irure a magna occaecat efflorescere, o amet incurreret expetendis, dolore non admodum."
     },
 ];
@@ -24,17 +24,16 @@ function seedDB() {
     Campground.remove({}, function(err) {
         if (err) {
             console.log(err);
-        } console.log("Campgrounds cleared.");
+        } console.log("Cleared campgrounds.");
 
         data.forEach(function(seed) {
             Campground.create(seed, function(err, campground) {
                 if (err) {
                     console.log(data);
                 } else {
-                    console.log("Campground seeded.");
+                    console.log("Seeded campground.");
 
-                    Comment.create(
-                        {
+                    Comment.create({
                             text: "No internet here.",
                             author: "Homer"
                         }, function(err, comment) {
